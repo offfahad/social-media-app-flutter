78d191021b540ea9ed72ce6395e3af6e6e03fe29
offfahad <mughalfahad544@gmail.com>
2023-09-26 17:01:21 +0500

    data-class-added

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,21 +1,21 @@
 import 'package:social_media_app/models/post_model.dart';
 
 class User {
-  final String profileImageUrl;
-  final String backgroundImageUrl;
-  final String name;
-  final int following;
-  final int followers;
-  final List<Post> posts;
-  final List<Post> favorites;
+  final String? profileImageUrl;
+  final String? backgroundImageUrl;
+  final String? name;
+  final int? following;
+  final int? followers;
+  final List<Post>? posts;
+  final List<Post>? favorites;
 
   User({
-    required this.profileImageUrl,
-    required this.backgroundImageUrl,
-    required this.name,
-    required this.following,
-    required this.followers,
-    required this.posts,
-    required this.favorites,
+    this.profileImageUrl,
+    this.backgroundImageUrl,
+    this.name,
+    this.following,
+    this.followers,
+    this.posts,
+    this.favorites,
   });
 }
