8b257bf1b67d58df5914e533b3ce5278c04b7841
offfahad <mughalfahad544@gmail.com>
2023-09-27 13:38:04 +0500

    bottom-tab-done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,9 +7,48 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Text(
+            'FREZY',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 10.0,
+            ),
+          ),
+          bottom: TabBar(
+            controller: _tabController,
+            labelColor: Theme.of(context).primaryColor,
+            labelStyle: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600,),
+            unselectedLabelStyle: const TextStyle(fontSize: 18.0),
+            indicatorWeight: 3.0,
+            tabs: const [
+              Tab(
+                text: 'Trending',
+              ),
+              Tab(
+                text: 'Latest',
+              )
+            ],
+          ),
+          actions: []),
+    );
   }
 }
