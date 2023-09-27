83c45b99d1f74db79a5014ac09a597a252abdd94
offfahad <mughalfahad544@gmail.com>
2023-09-27 14:50:47 +0500

    posts-half-done

--- lib/widgets/post_carousel.dart
+++ lib/widgets/post_carousel.dart
@@ -3,15 +3,70 @@ import 'package:social_media_app/data/data.dart';
 import 'package:social_media_app/models/post_model.dart';
 
 class PostCarousel extends StatelessWidget {
-  const PostCarousel({required this.pageController, required this.title, required this.posts,super.key});
+  const PostCarousel(
+      {required this.pageController,
+      required this.title,
+      required this.posts,
+      super.key});
 
   final PageController pageController;
   final String title;
   final List<Post> posts;
 
+  _buildPost(BuildContext context, int index) {
+    Post post = posts[index];
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15.0),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black26,
+                offset: Offset(0, 2),
+                blurRadius: 6.0,
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image(
+              height: 400.0,
+              width: 300.0,
+              image: AssetImage(post.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        )
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Padding(
+        padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+          ),
+        ),
+      ),
+      Container(
+        height: 400.0,
+        child: PageView.builder(
+          controller: pageController,
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return _buildPost(context, index);
+          },
+        ),
+      )
+    ]);
   }
 }
