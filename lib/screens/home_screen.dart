75ee078ec6db4332fd6416047c7673b6e44db15e
offfahad <mughalfahad544@gmail.com>
2023-09-27 13:58:55 +0500

    following-users-listview

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_app/data/data.dart';
+import 'package:social_media_app/models/user_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -21,35 +23,90 @@ class _HomeScreenState extends State<HomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        centerTitle: true,
+        title: Text(
+          'FREZY',
+          style: TextStyle(
             color: Theme.of(context).primaryColor,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 10.0,
           ),
-          centerTitle: true,
-          title: Text(
-            'FREZY',
-            style: TextStyle(
-              color: Theme.of(context).primaryColor,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 10.0,
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+          ),
+          unselectedLabelStyle: const TextStyle(fontSize: 18.0),
+          indicatorWeight: 3.0,
+          tabs: const [
+            Tab(
+              text: 'Trending',
             ),
-          ),
-          bottom: TabBar(
-            controller: _tabController,
-            labelColor: Theme.of(context).primaryColor,
-            labelStyle: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600,),
-            unselectedLabelStyle: const TextStyle(fontSize: 18.0),
-            indicatorWeight: 3.0,
-            tabs: const [
-              Tab(
-                text: 'Trending',
+            Tab(
+              text: 'Latest',
+            )
+          ],
+        ),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 10.0),
+                child: Text(
+                  'Following',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                  ),
+                ),
               ),
-              Tab(
-                text: 'Latest',
-              )
+              SizedBox(
+                height: 80.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: users.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    User user = users[index];
+                    return Container(
+                      margin: const EdgeInsets.all(10.0),
+                      height: 60.0,
+                      width: 60.0,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              offset: Offset(0, 2),
+                              blurRadius: 6.0,
+                            ),
+                          ]),
+                      child: ClipOval(
+                        child: Image(
+                          height: 60.0,
+                          width: 60.0,
+                          image: AssetImage(user.profileImageUrl!),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
-          actions: []),
+        ],
+      ),
     );
   }
 }
